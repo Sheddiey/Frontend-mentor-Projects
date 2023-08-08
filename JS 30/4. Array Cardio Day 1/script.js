8a56349bcdd1081a4d6e1ee95a8402eec79a37e6
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-08-08 16:55:07 +0300

    filter()

---
+++ JS 30/4. Array Cardio Day 1/script.js
@@ -0,0 +1,46 @@
+const inventors = [
+    {first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
+    {first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
+    {first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
+    {first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
+    {first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
+    {first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
+    {first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
+];
+
+
+const people = [ 'Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick',
+    'Beecher, Henry', 'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire',
+    'Bellow, Saul', 'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David', 
+    'Benjamin, Walter', 'Benn, Tony', 'Bennington, Chester', 'Benson, Leana', 'Bent, Silas',
+    'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar', 'Berio, Luciano', 'Berle, Milton',
+    'Berlin, Irving', 'Berne, Eric', 'Bernhard, Sandra', 'Berra, Yogi', 'Berry, Halle',
+    'Berry, Wendell', 'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph',
+    'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank', 'Birrell, Augustine',
+    'Black, Elk', 'Blair, Robert', 'Blair, Tony', 'Blake, William'
+];
+
+    //Array.prototype.filter()
+    //1. Filter the list of inventors for those who were born in the 1500's
+    const fifteen = inventors.filter(function(inventor) {
+        if(inventor.year >= 1500 && inventor.year < 1600) {
+            return true; //keep it
+        }
+    });
+    console.table(fifteen);
+    //Array.prototype.map()
+    //2.Give Us an Array of the inventors First and Last names
+
+    //Array.prototype.sort()
+    //3. Sort the inventors by birthdate, oldest to the youngest
+
+    //Array.prototype.reduce()
+    //4. How many years did all the inventors live
+
+    //5. sort the inventors by years lived
+
+    //6.Create a list of Boulverands in paris that contain 'de' anywhere in the name
+    //https://en.wikipedia.org/wiki/category:Boulevards_in_Paris
+
+    //7. sort exercise
+
