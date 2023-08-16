faad6e8ee62337dda97c3b2df07ac2a570ca4f33
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-08-16 19:12:00 +0300

    added evebt-listener

--- interactive-rating/main.js
+++ interactive-rating/main.js
@@ -1,8 +1,31 @@
-const thankYou = document.querySelector('.box-2');
-const rate = document.querySelector('.box-1');
+const buttons = document.querySelectorAll('.buttons button');
+const selectedRating = document.querySelector('.selected-rating');
+const error = document.querySelector('.eror-msg')
+const box2 = document.querySelector('.box-2');
+const box1 = document.querySelector('.box-1');
 const submit = document.querySelector('.submit');
+let selected = "";
+
+butttons.forEach( btn => {
+  btn.addEventListener('click', e => {
+    removeSelectedClasses();
+    selected = e.target.textContent
+    e.target.classList.add ("selected")
+  })
+});
 
 submit.addEventListener('click', e=> {
-  thankYou.style.display = 'block';
-  rate.style.display = 'none'
+  if(selected === "") {
+    addButtonErrors()
+    setTimeout(() => {
+      removeButtonErrors()
+    }, 3000);
+    return
+  }
+
+  selectedRating.textContent = `You selected ${selected} out of 5`
+  box1.classList.add('hide');
+  box2.classList.remove('hide')
 });
+
+
