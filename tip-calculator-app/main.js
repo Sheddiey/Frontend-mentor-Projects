c64271d00e13258fb5fa31615ec714b511ca8d6d
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-08-23 14:18:59 +0300

    dom elements

--- tip-calculator-app/main.js
+++ tip-calculator-app/main.js
@@ -1,13 +1,23 @@
-const inputs = document.querySelectorAll('input:not([name="custom"])');
+// Dom elements
+const inputs = document.querySelectorAll('input');
+const inputBill = document.querySelector('.bill');
+const inputCustom = document.querySelector('.input-tip');
+const inputPeople = document.querySelector('.number-people');
+const btn_5 = document.querySelector('.btn_5');
+const btn_10 = document.querySelector('.btn_10');
+const btn_15 = document.querySelector('.btn_15');
+const btn_25 = document.querySelector('.btn_25');
+const btn_50 = document.querySelector('.btn_50');
+const btnReset = document.querySelector('.btn-reset');
+const showTip = document.querySelector('.show-tip');
+const showTotal = document.querySelector('.show-total');
 const errorMessage = document.querySelector('.error');
 const errorMessage1 = document.querySelector('.error1');
 
-const bill = document.querySelector('.bill');
-const inputTipPerc = document.querySelector('.input-tip');
-const numberOfPeople = document.querySelector('.number-people');
 
-    bill.addEventListener('input', () => {
-        const inputValue1 = parseFloat(bill.value);
+
+    inputBill.addEventListener('input', () => {
+        const inputValue1 = parseFloat(inputBill.value);
 
         if(inputValue1 === 0) {
             errorMessage.classList.remove('hidden')
@@ -16,8 +26,8 @@ const numberOfPeople = document.querySelector('.number-people');
         }
     });
 
-    numberOfPeople.addEventListener('input', () => {
-        const inputValue2 = parseFloat(numberOfPeople.value)
+    inputPeoplePeople.addEventListener('input', () => {
+        const inputValue2 = parseFloat(inputPeoplePeople.value)
 
         if(inputValue2 === 0) {
             errorMessage1.classList.remove('hidden1')
