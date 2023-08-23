cc1a2f7060723ecc8bab22fd23bf8521b68aec52
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-08-24 02:27:32 +0300

    added navbar

---
+++ react-website/src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react'
+
+function Navbar() {
+  return (
+    <>
+    <nav className='navbar'>
+        <div className='navbar-container'>
+            <link to="/" className='navbar-logo'>
+                TRVL
+            </link>
+        </div>
+
+    </nav>
+    </>
+  )
+}
+
+export default Navbar
