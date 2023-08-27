e88f0c1b0e2c4d3be9bcd948c14330bd0c746ba2
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-08-27 03:36:29 +0300

    handlin events

--- my-app/src/components/MyButton.jsx
+++ my-app/src/components/MyButton.jsx
@@ -1,6 +1,15 @@
 function MyButton () {
+    function handleClick () {
+        alert('You clicked me!')
+    }
+
     return (
+        <>        
         <button>I am a button</button>
+        <button onClick={handleClick}>
+            Click me.
+        </button>
+        </>
     );
 }
 
