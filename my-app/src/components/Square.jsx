b56db2fd349bf89fd57710bf264bd67cdd5f324d
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-08-28 12:27:19 +0300

    interactivity

--- my-app/src/components/Square.jsx
+++ my-app/src/components/Square.jsx
@@ -1,6 +1,19 @@
-function Square({ value }) {
-    
-    return <button className="square">{ value }</button>
+import { useState } from "react";
+
+function Square() {
+    const [value, setValue] = useState(null)
+
+    function handleClick() {
+        setValue('X')
+    }
+    return (
+        <button 
+            className="square"
+            onClick={handleClick}
+        >
+            { value }
+        </button>
+    ); 
 }
 
 export default Square;
