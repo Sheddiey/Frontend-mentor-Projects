b06f2ac7a813c47ac412a4876278aeeaa296c126
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-08-28 13:19:13 +0300

    lifting up state

--- my-app/src/components/Square.jsx
+++ my-app/src/components/Square.jsx
@@ -1,19 +1,9 @@
-import { useState } from "react";
-
-function Square() {
-    const [value, setValue] = useState(null)
-
-    function handleClick() {
-        setValue('X')
-    }
-    return (
-        <button 
-            className="square"
-            onClick={handleClick}
-        >
-            { value }
-        </button>
-    ); 
+function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
 }
 
 export default Square;
