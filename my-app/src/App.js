a3d2776649d27b4e8b49b952a48acc34a9f9bea8
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-08-28 11:24:01 +0300

    tic tac toe game initial commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,2 @@
-import ListItemsFunction from "./components/ListItems";
-import MyButton from "./components/MyButton";
-import MycounterButton from "./components/MyCounterButton";
-import { useState } from "react";
 
-export default function MyApp () {
-  const [count, setCount] = useState(0)
-
-    function handleClick2 () {
-        setCount(count + 1);
-    }
-
-  return (
-    <div>
-      <h1>Welcome to My App</h1>
-      <MyButton/>
-      <ListItemsFunction/>
-      <h1>Counters that update together</h1>
-      <MycounterButton count={count} onClick = {handleClick2}/>
-      <MycounterButton count={count} onClick = {handleClick2}/>
-    </div>
-  );
-}
 
