9d83a579891f0fa03f2ae01eaaeec0d6d2664fd3
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-08-31 13:34:09 +0300

    a working tic-tac-toe game

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -18,8 +18,16 @@ export default function Board() {
     setSquares(nextSquares);
     setXIsNext(!xIsNext);
   }
+  const winner = calculateWinner(squares);
+  let status;
+  if(winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
   return(
     <>
+    <div className="status">{ status }</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
