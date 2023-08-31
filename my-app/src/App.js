47643c6d96705a2f0bac34ad1c7647e59f139aec
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-08-31 13:27:18 +0300

    determining the winner

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,7 +6,7 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
-    if(squares[i]) {
+    if(squares[i] || calculateWinner(squares)) {
       return;
     }
     const nextSquares = squares.slice();
@@ -39,5 +39,25 @@ export default function Board() {
     </>
   )
   
+}
+
+function calculateWinner (squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] =lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
