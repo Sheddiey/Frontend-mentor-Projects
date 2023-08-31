af58bb4e46361d7607dccfaa8028fde80a8cb070
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-08-31 13:06:55 +0300

    taking turns

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,21 @@ import { useState } from "react";
 import Square from "./components/Square";
 
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
+    if(squares[i]) {
+      return;
+    }
     const nextSquares = squares.slice();
-    nextSquares[i] ='X';
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
   return(
     <>
