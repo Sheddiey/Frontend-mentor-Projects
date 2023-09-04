78e2d3b6957b851ce97bfe24457abdc7d3fc6718
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-09-04 06:53:19 +0300

    props to purify component

--- my-appp/src/App.js
+++ my-appp/src/App.js
@@ -1,17 +1,14 @@
-let guest = 0;
 
-function Cup () {
-  //bad: changing a pre existing variable
-  guest = guest + 1;
+function Cup ({ guest }) {
   return <h2>Tea cup for guest #{guest}</h2>
 }
 
 export default function TeaSet() {
   return (
     <>
-      <Cup />
-      <Cup />
-      <Cup />
+      <Cup guest={1}/>
+      <Cup guest={2}/>
+      <Cup guest={3}/>
     </>
   );
 }
