35c3a0d7ed89765ccf23a5e9cb86e80966ca6b5c
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-09-04 07:14:38 +0300

    pure cmponents

--- my-appp/src/App.js
+++ my-appp/src/App.js
@@ -1,14 +1,23 @@
-
-function Cup ({ guest }) {
-  return <h2>Tea cup for guest #{guest}</h2>
+function Recipe ({ drinkers }) {
+  return (
+    <ol>
+      <li>Boil {drinkers} cups of water</li>
+      <li>Add {drinkers} spoons of tea and {0.5 * drinkers} spoons of spice</li>
+      <li>Add {0.5 * drinkers} cup of milk to boil and sugar to taste</li>
+    </ol>
+  );
 }
 
-export default function TeaSet() {
+export default function App() {
   return (
-    <>
-      <Cup guest={1}/>
-      <Cup guest={2}/>
-      <Cup guest={3}/>
-    </>
+    <section>
+      <h1>Spiced Chai recipe</h1>
+      <h2>For two</h2>
+      <Recipe drinkers={2} />
+      <h2>For a gathering</h2>
+      <Recipe drinkers={4} />
+      <h2>For a Ruracio</h2>
+      <Recipe drinkers={10} />
+    </section>
   );
 }
