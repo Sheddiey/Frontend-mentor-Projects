fd56a3d9acf03b231b7e36bff658277effcb4d19
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-09-05 19:34:13 +0300

    state is isolated and private

--- my-appp/src/App.js
+++ my-appp/src/App.js
@@ -1,39 +1,10 @@
-import { useState } from "react";
-import { sculptureList } from "./data";
+import Gallery from "./Gallery";
 
-export default function Gallery() {
-  const [index, setIndex] = useState(0);
-  const [showMore, setShowMore] = useState(false);
-
-  function handleClick() {
-    setIndex(index + 1);
-  }
-
-  function handleMoreClick() {
-    setShowMore(!showMore);
-  }
-
-  let sculpture = sculptureList[index];
+export default function App() {
   return (
     <>
-      <button onClick={handleClick}>
-        Next
-      </button>
-      <h2>
-        <i>{sculpture.name} </i>
-        by {sculpture.artist}
-      </h2>
-      <h3>
-        ({index + 1} of {sculptureList.length})
-      </h3>
-      <button onClick={handleMoreClick}>
-        {showMore ? 'hide' : 'show'} details
-      </button>
-      {showMore && <p>{sculpture.description}</p>}
-      <img 
-        src={sculpture.url}
-        alt={sculpture.alt}
-      />
+      <Gallery />
+      <Gallery />
     </>
   );
 }
---
+++ my-appp/src/Gallery.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { sculptureList } from "./data";
+
+export default function Gallery() {
+  const [index, setIndex] = useState(0);
+  const [showMore, setShowMore] = useState(false);
+
+  function handleClick() {
+    setIndex(index + 1);
+  }
+
+  function handleMoreClick() {
+    setShowMore(!showMore);
+  }
+
+  let sculpture = sculptureList[index];
+  return (
+    <>
+      <button onClick={handleClick}>
+        Next
+      </button>
+      <h2>
+        <i>{sculpture.name} </i>
+        by {sculpture.artist}
+      </h2>
+      <h3>
+        ({index + 1} of {sculptureList.length})
+      </h3>
+      <button onClick={handleMoreClick}>
+        {showMore ? 'hide' : 'show'} details
+      </button>
+      {showMore && <p>{sculpture.description}</p>}
+      <img 
+        src={sculpture.url}
+        alt={sculpture.alt}
+      />
+    </>
+  );
+}
