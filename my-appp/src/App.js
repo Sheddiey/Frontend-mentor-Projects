08d7e62c7f459c9b7146661932e0bf338ce0a3b2
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-09-05 17:20:42 +0300

    form default behaviour

--- my-appp/src/App.js
+++ my-appp/src/App.js
@@ -1,26 +1,8 @@
-function Button({ onClick, children }) {
+export default function Signup() {
   return (
-    <button
-      onClick={(e) => {
-        e.stopPropagation();
-        onClick();
-      }}
-    >
-      {children}
-    </button>
-  );
-}
-
-export default function Toolbar() {
-  return (
-    <div
-      className="toolbar"
-      onClick={() => {
-        alert("You clicked on the toolbar!");
-      }}
-    >
-      <Button onClick={() => alert("Playing!")}>Play movie</Button>
-      <Button onClick={() => alert("Uploading!")}>Upload image</Button>
-    </div>
+    <form className="toolbar" onSubmit={() => alert("Submitting!")}>
+      <input />
+      <button>send</button>
+    </form>
   );
 }
