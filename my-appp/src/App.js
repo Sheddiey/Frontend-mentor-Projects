2c87cc9bcb546d3d5b094ace8dba9a9772f3caf1
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-09-05 17:15:44 +0300

    stopping propagation

--- my-appp/src/App.js
+++ my-appp/src/App.js
@@ -1,14 +1,26 @@
+function Button({ onClick, children }) {
+  return (
+    <button
+      onClick={(e) => {
+        e.stopPropagation();
+        onClick();
+      }}
+    >
+      {children}
+    </button>
+  );
+}
+
 export default function Toolbar() {
   return (
-    <div className="toolbar" onClick={() => {
-      alert("You clicked on the toolbar!")
-    }}>
-      <button onClick={() => alert("Playing!")}>
-        Play movie
-      </button>
-      <button onClick={() => alert("Uploading!")}>
-        Upload image
-      </button>
+    <div
+      className="toolbar"
+      onClick={() => {
+        alert("You clicked on the toolbar!");
+      }}
+    >
+      <Button onClick={() => alert("Playing!")}>Play movie</Button>
+      <Button onClick={() => alert("Uploading!")}>Upload image</Button>
     </div>
   );
 }
