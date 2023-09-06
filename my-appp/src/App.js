e57fcc800f4fad67232fc0c8d747ace5ba275229
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-09-06 14:49:52 +0300

    fixing traffic lights

--- my-appp/src/App.js
+++ my-appp/src/App.js
@@ -1,35 +1,23 @@
 import { useState } from "react";
 
-export default function Form() {
-  const [to, setTo] = useState('Alice');
-  const [message, setMessage] = useState('Hello')
+export default function TrafficLight() {
+  const [walk, setWalk] = useState(true);
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    setTimeout(() => {
-      alert(`You said ${message} to ${to}`);
-    }, 5000);
+  function handleClick() {
+    setWalk(!walk);
+    alert(walk ? 'Stop is next' : 'Walk is next');
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        To:{' '}
-        <select
-          value={to}
-          onChange={e => setTo(e.target.value)}>
-          <option value='Alice'>Alice</option>
-          <option value='Bob mar'>Bob mar</option>
-          <option value='Bob mar'>Bob mar</option>
-          <option value='Bob mar'>Bob mar</option>
-        </select>
-      </label>
-      <textarea 
-        placeholder="Message"
-        value={message}
-        onChange={e => setMessage(e.target.value)}
-      />
-      <button type="submit">Send</button>
-    </form>
+    <>
+      <button onClick={handleClick}>Change to {walk ? "Stop" : "walk"}</button>
+      <h1
+        style={{
+          color: walk ? "darkgreen" : "darkred",
+        }}
+      >
+        {walk ? "Walk" : "Stop"}
+      </h1>
+    </>
   );
 }
