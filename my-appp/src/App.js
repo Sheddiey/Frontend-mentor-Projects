578fb522a8ad0f671525385f9adfca565bcdfe4f
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-09-06 14:31:33 +0300

    state over time

--- my-appp/src/App.js
+++ my-appp/src/App.js
@@ -1,21 +1,35 @@
 import { useState } from "react";
 
+export default function Form() {
+  const [to, setTo] = useState('Alice');
+  const [message, setMessage] = useState('Hello')
 
-export default function Counter() {
-  const [number, setNumber] = useState(0);
+  function handleSubmit(e) {
+    e.preventDefault();
+    setTimeout(() => {
+      alert(`You said ${message} to ${to}`);
+    }, 5000);
+  }
 
   return (
-    <>
-      <h1>{number}</h1>
-      <button onClick={() => {
-        setNumber(number + 5);
-        setTimeout(() => {
-          alert(number);
-        }, 3000);
-        
-      }}>
-        +5
-      </button>
-    </>
+    <form onSubmit={handleSubmit}>
+      <label>
+        To:{' '}
+        <select
+          value={to}
+          onChange={e => setTo(e.target.value)}>
+          <option value='Alice'>Alice</option>
+          <option value='Bob mar'>Bob mar</option>
+          <option value='Bob mar'>Bob mar</option>
+          <option value='Bob mar'>Bob mar</option>
+        </select>
+      </label>
+      <textarea 
+        placeholder="Message"
+        value={message}
+        onChange={e => setMessage(e.target.value)}
+      />
+      <button type="submit">Send</button>
+    </form>
   );
 }
