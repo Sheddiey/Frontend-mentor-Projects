6e00a9810d116462ffe178bd7c974d79b21504bb
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-09-06 14:16:59 +0300

    state overtime

--- my-appp/src/App.js
+++ my-appp/src/App.js
@@ -1,9 +1,21 @@
-import Gallery from "./Gallery";
+import { useState } from "react";
 
-export default function App() {
+
+export default function Counter() {
+  const [number, setNumber] = useState(0);
+
   return (
     <>
-      <Gallery />
+      <h1>{number}</h1>
+      <button onClick={() => {
+        setNumber(number + 5);
+        setTimeout(() => {
+          alert(number);
+        }, 3000);
+        
+      }}>
+        +5
+      </button>
     </>
   );
 }
