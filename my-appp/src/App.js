0381972dd98599e1a02b1ad9e4bf451e3669fa7a
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-09-07 11:42:29 +0300

    fixing a bug using spread operator

--- my-appp/src/App.js
+++ my-appp/src/App.js
@@ -1,39 +1,53 @@
-import Background from "./Background.js";
-import Box from "./Box.js";
-import { useImmer } from "use-immer";
+import { useState } from 'react';
+import Background from './Background.js';
+import Box from './Box.js';
 
 const initialPosition = {
   x: 0,
-  y: 0,
+  y: 0
 };
 
 export default function Canvas() {
-  const [shape, updateShape] = useImmer({
-    color: "orange",
-    position: initialPosition,
+  const [shape, setShape] = useState({
+    color: 'orange',
+    position: initialPosition
   });
 
   function handleMove(dx, dy) {
-    updateShape(draft => {
-      draft.position.x += dx;
-      draft.position.y += dy;
+    setShape({
+      ...shape,
+      position: {
+        x: shape.position.x + dx,
+        y: shape.position.y + dy
+      }
+    })
+  }
+  
+  function handleColorChange(e) {
+    setShape({
+      ...shape,
+      color: e.target.value
     });
   }
 
-  function handleColorChange(e) {
-    updateShape(draft => {
-      draft.color = e.target.value;
-    });
-  }
   return (
     <>
-      <select value={shape.color} onChange={handleColorChange}>
+      <select
+        value={shape.color}
+        onChange={handleColorChange}
+      >
         <option value="orange">orange</option>
         <option value="lightpink">lightpink</option>
         <option value="aliceblue">aliceblue</option>
       </select>
-      <Background position={initialPosition} />
-      <Box color={shape.color} position={shape.position} onMove={handleMove}>
+      <Background
+        position={initialPosition}
+      />
+      <Box
+        color={shape.color}
+        position={shape.position}
+        onMove={handleMove}
+      >
         Drag me!
       </Box>
     </>
