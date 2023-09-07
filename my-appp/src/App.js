4274c892a72a8233a161e31fcd266737fe94bbbd
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-09-07 14:04:21 +0300

    transforming an array

--- my-appp/src/App.js
+++ my-appp/src/App.js
@@ -1,31 +1,47 @@
 import { useState } from "react";
 
-let initialArtists = [
-  { id: 0, name: "Marta Colvin Andrade" },
-  { id: 1, name: "Lamidi Olonade Fakeye" },
-  { id: 2, name: "Louis Nevelson" },
+let initialShapes = [
+  { id: 0, type: 'circle', x: 50, y: 100, },
+  { id: 1, type: 'square', x: 150, y: 100, },
+  { id: 2, type: 'circle', x: 250, y: 100, },
 ];
 
-export default function List() {
-  const [artists, setArtists] = useState(initialArtists);
+export default function ShapeEditor() {
+  const [shapes, setShapes] = useState(initialShapes);
+
+  function handleClick() {
+    const nextShapes = shapes.map(shape => {
+      if (shape.type === 'square') {
+        return shape;
+      } else {
+        return {
+          ...shape,
+          y: shape.y + 50,
+        };
+      }
+    });
+    setShapes(nextShapes);
+  }
 
   return (
     <>
-      <h1>Inspiring Sculptors:</h1>
-      <ul>
-        {artists.map((artist) => (
-          <li key={artist.id}>
-            {artist.name}{" "}
-            <button
-              onClick={() => {
-                setArtists(artists.filter((a) => a.id !== artist.id));
-              }}
-            >
-              Delete
-            </button>
-          </li>
-        ))}
-      </ul>
+      <button onClick={handleClick}>
+        Move circle down!
+      </button>
+      {shapes.map(shape => (
+        <div
+          key={shape.id}
+          style={{
+            background: 'purple',
+            position: 'absolute',
+            left: shape.x,
+            top: shape.y,
+            borderRadius: 
+              shape.type === 'circle' ? '50%' : '',
+            width: 20,
+            height: 20,
+          }} />
+      ))}
     </>
   );
 }
