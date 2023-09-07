566eb09907eafc7412aa24c9d04507348fa5f1e2
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-09-07 21:36:31 +0300

    replacing an item in an array

--- my-appp/src/App.js
+++ my-appp/src/App.js
@@ -1,47 +1,33 @@
 import { useState } from "react";
 
-let initialShapes = [
-  { id: 0, type: 'circle', x: 50, y: 100, },
-  { id: 1, type: 'square', x: 150, y: 100, },
-  { id: 2, type: 'circle', x: 250, y: 100, },
+let initialCounters = [
+  0, 0, 0
 ];
 
-export default function ShapeEditor() {
-  const [shapes, setShapes] = useState(initialShapes);
+export default function CounterList() {
+  const [counters, setCounters] = useState(initialCounters);
 
-  function handleClick() {
-    const nextShapes = shapes.map(shape => {
-      if (shape.type === 'square') {
-        return shape;
+  function handleIncrementClick(index) {
+    const nextCounters = counters.map((c, i) => {
+      if (i === index) {
+        return c + 1;
       } else {
-        return {
-          ...shape,
-          y: shape.y + 50,
-        };
+        return c;
       }
     });
-    setShapes(nextShapes);
+    setCounters(nextCounters);
   }
 
   return (
-    <>
-      <button onClick={handleClick}>
-        Move circle down!
-      </button>
-      {shapes.map(shape => (
-        <div
-          key={shape.id}
-          style={{
-            background: 'purple',
-            position: 'absolute',
-            left: shape.x,
-            top: shape.y,
-            borderRadius: 
-              shape.type === 'circle' ? '50%' : '',
-            width: 20,
-            height: 20,
-          }} />
+    <ul>
+      {counters.map((counter, i) => (
+        <li key={i}>
+          {counter}
+          <button onClick={() => {
+            handleIncrementClick(i);
+          }}>+1</button>
+        </li>
       ))}
-    </>
+    </ul>
   );
 }
