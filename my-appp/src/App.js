2bc9c1be97ca413d54dcff4820da6c9fe3f54810
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-09-07 12:25:37 +0300

    adding to an array

--- my-appp/src/App.js
+++ my-appp/src/App.js
@@ -1,55 +1,29 @@
-import { useState } from 'react';
-import Background from './Background.js';
-import Box from './Box.js';
+import { useState } from "react";
 
-const initialPosition = {
-  x: 0,
-  y: 0
-};
+let nextId = 0;
 
-export default function Canvas() {
-  const [shape, setShape] = useState({
-    color: 'orange',
-    position: initialPosition
-  });
-
-  function handleMove(dx, dy) {
-    setShape({
-      ...shape,
-      position: {
-        x: shape.position.x + dx,
-        y: shape.position.y + dy
-      }
-    })
-  }
-  
-  function handleColorChange(e) {
-    setShape({
-      ...shape,
-      color: e.target.value
-    });
-  }
+export default function List() {
+  const [name, setName] = useState('');
+  const [artists, setArtists] = useState([]);
 
   return (
     <>
-      <select
-        value={shape.color}
-        onChange={handleColorChange}
-      >
-        <option value="orange">orange</option>
-        <option value="lightpink">lightpink</option>
-        <option value="aliceblue">aliceblue</option>
-      </select>
-      <Background
-        position={initialPosition}
+      <h1>Inspiring Sculptors</h1>
+      <input 
+        value={name}
+        onChange={e => setName(e.target.value)}
       />
-      <Box
-        color={shape.color}
-        position={shape.position}
-        onMove={handleMove}
-      >
-        Drag me!
-      </Box>
+      <button onClick={() => {
+        artists.push({
+          id: nextId++,
+          name: name,
+        });
+      }}>Add</button>
+      <ul>
+        {artists.map(artist => (
+          <li key={artist.id}>{artist.name}</li>
+        ))}
+      </ul>
     </>
   );
 }
