3840c157656ae513afff795db19ad9dd14b324f2
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-09-08 09:58:42 +0300

    inserting into an array

--- my-appp/src/App.js
+++ my-appp/src/App.js
@@ -1,33 +1,41 @@
 import { useState } from "react";
 
-let initialCounters = [
-  0, 0, 0
+let nextId = 3;
+const initialArtists = [
+  { id: 0, name: 'Marta Colvin Andrade'},
+  { id: 1, name: 'Lamidi Olonade Fakeye'},
+  { id: 2, name: 'Louise Nevelson'},
 ];
 
-export default function CounterList() {
-  const [counters, setCounters] = useState(initialCounters);
+export default function List() {
+  const [name, setName] = useState('');
+  const [artists, setArtists] = useState(initialArtists);
 
-  function handleIncrementClick(index) {
-    const nextCounters = counters.map((c, i) => {
-      if (i === index) {
-        return c + 1;
-      } else {
-        return c;
-      }
-    });
-    setCounters(nextCounters);
+  function handleClick() {
+    const insertAt =1;
+
+  const nextArtists = [
+    ...artists.slice(0, insertAt),
+    { id: nextId++, name: name},
+    ...artists.slice(insertAt)
+  ];
+  setArtists(nextArtists);
+  setName('')
   }
 
   return (
-    <ul>
-      {counters.map((counter, i) => (
-        <li key={i}>
-          {counter}
-          <button onClick={() => {
-            handleIncrementClick(i);
-          }}>+1</button>
-        </li>
-      ))}
-    </ul>
+    <>
+      <h1>Inspiring Sculptors</h1>
+      <input
+        value={name}
+        onChange={e => setName(e.target.value)}
+      />
+      <button onClick={handleClick}>Insert</button>
+      <ul>
+        {artists.map(artist => (
+          <li key={artist.id}>{artist.name}</li>
+        ))}
+      </ul>
+    </>
   );
 }
