f79b1658c7fcc93ee60e75c8328319645cbd0830
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-09-08 10:08:18 +0300

    using the reverse method

--- my-appp/src/App.js
+++ my-appp/src/App.js
@@ -1,39 +1,29 @@
 import { useState } from "react";
 
 let nextId = 3;
-const initialArtists = [
-  { id: 0, name: 'Marta Colvin Andrade'},
-  { id: 1, name: 'Lamidi Olonade Fakeye'},
-  { id: 2, name: 'Louise Nevelson'},
+const initialList = [
+  { id: 0, title: 'Big Bellies' },
+  { id: 1, title: 'Lunar Landscape' },
+  { id: 2, title: 'Terracotta Army' },
 ];
 
 export default function List() {
-  const [name, setName] = useState('');
-  const [artists, setArtists] = useState(initialArtists);
+  const [list, setList] = useState(initialList);
 
   function handleClick() {
-    const insertAt =1;
-
-  const nextArtists = [
-    ...artists.slice(0, insertAt),
-    { id: nextId++, name: name},
-    ...artists.slice(insertAt)
-  ];
-  setArtists(nextArtists);
-  setName('')
+    const nextList = [...list];
+    nextList.reverse();
+    setList(nextList);
   }
 
   return (
     <>
-      <h1>Inspiring Sculptors</h1>
-      <input
-        value={name}
-        onChange={e => setName(e.target.value)}
-      />
-      <button onClick={handleClick}>Insert</button>
+      <button onClick={handleClick}>
+        Reverse
+      </button>
       <ul>
-        {artists.map(artist => (
-          <li key={artist.id}>{artist.name}</li>
+        {list.map(artwork => (
+          <li key={artwork.id}>{artwork.title}</li>
         ))}
       </ul>
     </>
