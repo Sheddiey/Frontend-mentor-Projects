c1ae9691e6b402ecf8c4089d7bc49441bfca21ea
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-09-11 17:47:27 +0300

    removed redundancy

--- my-app/src/Managing state/Changing-State/Form.jsx
+++ my-app/src/Managing state/Changing-State/Form.jsx
@@ -3,16 +3,16 @@ import { useState } from "react";
 export default function Form() {
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
-    const [fullName, setFullName] = useState('');
+
+
+    const fullName = firstName + ' ' + lastName;
 
     function handleFirstNameChange(e) {
         setFirstName(e.target.value);
-        setFullName(e.target.value + ' ' + lastName);
     }
 
     function handleLastNameChange(e) {
         setLastName(e.target.value);
-        setFullName(firstName + ' ' + e.target.value)
     }
 
     return (
