a0fb2d5c6a5140fbc500a079a96a45d07fbec983
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-09-11 05:04:32 +0300

    updating objects inside arrays

--- my-appp/src/App.js
+++ my-appp/src/App.js
@@ -1,34 +1,35 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 let nextId = 3;
+
 const initialList = [
-  { id: 0, title: 'Big Bellies', seen: false },
-  { id: 1, title: 'Lunar Landscape', seen: false },
-  { id: 2, title: 'Terracotta Army', seen: true },
+  { id: 0, title: "Big Bellies", seen: false },
+  { id: 1, title: "Lunar Lanscape", seen: false },
+  { id: 2, title: "Terracotta Army", seen: true },
 ];
 
 export default function BucketList() {
   const [myList, setMyList] = useState(initialList);
-  const [yourList, setYourList] = useState(
-    initialList
-  );
+  const [yourList, setYourList] = useState(initialList);
 
   function handleToggleMyList(artworkId, nextSeen) {
-    const myNextList = [...myList];
-    const artwork = myNextList.find(
-      a => a.id === artworkId
-    );
-    artwork.seen = nextSeen;
-    setMyList(myNextList);
+    setMyList(myList.map(artwork => {
+      if (artwork.id === artworkId) {
+        return {...artwork, seen: nextSeen};
+      } else {
+        return artwork;
+      }
+    }));
   }
 
   function handleToggleYourList(artworkId, nextSeen) {
-    const yourNextList = [...yourList];
-    const artwork = yourNextList.find(
-      a => a.id === artworkId
-    );
-    artwork.seen = nextSeen;
-    setYourList(yourNextList);
+    setYourList(yourList.map(artwork => {
+      if (artwork.id === artworkId) {
+        return { ...artwork, seen: nextSeen};
+      } else {
+        return artwork;
+      }
+    }));
   }
 
   return (
@@ -37,11 +38,13 @@ export default function BucketList() {
       <h2>My list of art to see:</h2>
       <ItemList
         artworks={myList}
-        onToggle={handleToggleMyList} />
+        onToggle={handleToggleMyList}
+      />
       <h2>Your list of art to see:</h2>
       <ItemList
         artworks={yourList}
-        onToggle={handleToggleYourList} />
+        onToggle={handleToggleYourList}
+      />
     </>
   );
 }
@@ -49,17 +52,14 @@ export default function BucketList() {
 function ItemList({ artworks, onToggle }) {
   return (
     <ul>
-      {artworks.map(artwork => (
+      {artworks.map((artwork) => (
         <li key={artwork.id}>
           <label>
             <input
               type="checkbox"
               checked={artwork.seen}
-              onChange={e => {
-                onToggle(
-                  artwork.id,
-                  e.target.checked
-                );
+              onChange={(e) => {
+                onToggle(artwork.id, e.target.checked);
               }}
             />
             {artwork.title}
