c1cb73ef480658314b315e9233483dd27bf4e18b
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-09-11 18:20:32 +0300

    removed duplication in state

--- my-app/src/Managing state/Changing-State/Menu.jsx
+++ my-app/src/Managing state/Changing-State/Menu.jsx
@@ -8,18 +8,36 @@ const initialItems = [
 
 export default function Menu() {
     const [items, setItems] = useState(initialItems);
-    const [selectedItem, setSelectedItem] = useState(items[0]);
+    const [selectedId, setSelectedId] = useState(0);
+
+    const selectedItem = items.find(item => item.id === selectedId);
+
+    function handleItemChange(id, e) {
+        setItems(items.map(item => {
+            if (item.id === id) {
+                return {
+                    ...item,
+                    title: e.target.value,
+                };
+            } else {
+                return item;
+            }
+        }));
+    }
 
     return (
         <>
             <h2>Whats your travel snack.</h2>
             <ul>
-                {items.map(item => (
+                {items.map((item, index) => (
                     <li key={item.id}>
-                        {item.title}
-                        {' '}
+                        <input 
+                            value={item.title}
+                            onChange={(e) => {handleItemChange(item.id, e)}}
+
+                        />
                         <button onClick={() => {
-                            setSelectedItem(item);
+                            setSelectedId(item);
                         }}>Choose</button>
                     </li>
                 ))}
