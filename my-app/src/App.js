49d2b4bf2192fa66c2e75bbf18f6f1288e637b22
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-09-12 11:34:08 +0300

    a chat app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
-import Scoreboard from "./Managing state/Preserving-Resetting-State/Scoreboard";
+import Messenger from "./Managing state/Preserving-Resetting-State/Messenger";
 
 export default function App() {
-  return <Scoreboard />
+  return <Messenger />
 }
