0996cbc77ef94ca4843449210f703684aa4900ae
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-09-12 15:05:10 +0300

    writing concise reducers with immer

--- my-app/src/Managing state/State-logic-reducer/TaskApp.jsx
+++ my-app/src/Managing state/State-logic-reducer/TaskApp.jsx
@@ -1,9 +1,9 @@
-import { useReducer } from "react";
+import { useImmerReducer } from "react";
 import AddTask from "./AddTask";
 import TaskList from "./TaskList";
 
 export default function TaskApp() {
-  const [tasks, dispatch] = useReducer(tasksReducer, initialTasks);
+  const [tasks, dispatch] = useImmerReducer(tasksReducer, initialTasks);
 
   function handleAddTask(text) {
     dispatch({
@@ -40,29 +40,23 @@ export default function TaskApp() {
   );
 }
 
-function tasksReducer(tasks, action) {
+function tasksReducer(draft, action) {
   switch (action.type) {
     case "added": {
-      return [
-        {
-          id: action.id,
-          text: action.text,
-          done: false,
-        },
-        ...tasks,
-      ];
+      draft.push({
+        id: action.id,
+        text: action.text,
+        done: false,
+      });
+      break;
     }
     case "changed": {
-      return tasks.map((t) => {
-        if (t.id === action.task.id) {
-          return action.task;
-        } else {
-          return t;
-        }
-      });
+      const index = draft.findIndex((t) => t.id === action.task.id);
+      draft[index] = action.task;
+      break;
     }
     case "deleted": {
-      return tasks.filter((t) => t.id !== action.id);
+      return draft.filter((t) => t.id !== action.id);
     }
     default: {
       throw Error("Unknown action: " + action.type);
