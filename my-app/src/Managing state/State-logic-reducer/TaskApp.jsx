89201a5a1679e648c2eae9231a7e38d4dee903b8
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-09-12 14:47:44 +0300

    extracted state logic into a reducer

--- my-app/src/Managing state/State-logic-reducer/TaskApp.jsx
+++ my-app/src/Managing state/State-logic-reducer/TaskApp.jsx
@@ -1,55 +1,78 @@
-import { useState } from "react";
+import { useReducer } from "react";
 import AddTask from "./AddTask";
 import TaskList from "./TaskList";
 
 export default function TaskApp() {
-    const [tasks, setTasks] = useState(initialTasks);
+  const [tasks, dispatch] = useReducer(tasksReducer, initialTasks);
 
-    function handleAddTask(text) {
-        setTasks([
-            ...tasks,
-            {
-                id: nextId++,
-                text: text,
-                done: false,
-            },
-        ]);
+  function handleAddTask(text) {
+    dispatch({
+      type: "added",
+      id: nextId++,
+      text: text,
+    });
+  }
+
+  function handleChangeTask(task) {
+    dispatch({
+      type: "changed",
+      task: task,
+    });
+  }
+
+  function handleDeleteTask(taskId) {
+    dispatch({
+      type: "deleted",
+      id: taskId,
+    });
+  }
+
+  return (
+    <>
+      <h1>Prague itinerary</h1>
+      <AddTask onAddTask={handleAddTask} />
+      <TaskList
+        tasks={tasks}
+        onChangeTask={handleChangeTask}
+        onDeleteTask={handleDeleteTask}
+      />
+    </>
+  );
+}
+
+function tasksReducer(tasks, action) {
+  switch (action.type) {
+    case "added": {
+      return [
+        {
+          id: action.id,
+          text: action.text,
+          done: false,
+        },
+        ...tasks,
+      ];
     }
-
-    function handleChangeTask(task) {
-        setTasks(
-            tasks.map((t) => {
-                if (t.id === task.id) {
-                    return task;
-                } else {
-                    return t;
-                }
-            })
-        );
+    case "changed": {
+      return tasks.map((t) => {
+        if (t.id === action.task.id) {
+          return action.task;
+        } else {
+          return t;
+        }
+      });
     }
-
-    function handleDeleteTask(taskId) {
-        setTasks(tasks.filter((t) => t.id !== taskId))
+    case "deleted": {
+      return tasks.filter((t) => t.id !== action.id);
     }
-
-    return (
-        <>
-            <h1>Prague itinerary</h1>
-            <AddTask onAddTask={handleAddTask} />
-            <TaskList
-                tasks={tasks}
-                onChangeTask={handleChangeTask}
-                onDeleteTask={handleDeleteTask}
-            />
-        </>
-    );
-
-
+    default: {
+      throw Error("Unknown action: " + action.type);
+    }
+  }
 }
 
 let nextId = 3;
 const initialTasks = [
-    {id: 0, text: 'Visit Kafka Museum', done: true},
-    {id: 1, text: 'watch a puppet show', done: false},
-    {id: 2, text: 'lennon wall pic', done: false},
-]
+  { id: 0, text: "Visit Kafka Museum", done: true },
+  { id: 1, text: "watch a puppet show", done: false },
+  { id: 2, text: "lennon wall pic", done: false },
+];
