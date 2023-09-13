6e81ef1daa7b76d59fa5b78946e7f56bec5d9bd0
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-09-13 12:21:37 +0300

    multiple headings within the same section

--- my-app/src/Managing state/Passing-data-deeply-context/Page.jsx
+++ my-app/src/Managing state/Passing-data-deeply-context/Page.jsx
@@ -2,14 +2,34 @@ import Heading from "./Heading";
 import Section from "./Section";
 
 export default function Page() {
-    return (
+  return (
+    <Section>
+      <Heading level={1}>Title</Heading>
+      <Section>
+        <Heading level={2}>Heading</Heading>
+        <Heading level={2}>Heading</Heading>
+        <Heading level={2}>Heading</Heading>
         <Section>
-            <Heading level={1}>Title</Heading>
-            <Heading level={2}>Heading</Heading>
-            <Heading level={3}>Sub-heading</Heading>
+          <Heading level={3}>Sub-heading</Heading>
+          <Heading level={3}>Sub-heading</Heading>
+          <Heading level={3}>Sub-heading</Heading>
+          <Section>
             <Heading level={4}>Sub-sub-heading</Heading>
-            <Heading level={5}>Sub-sub-sub-heading</Heading>
-            <Heading level={6}>Sub-sub-sub-sub-heading</Heading>
+            <Heading level={4}>Sub-sub-heading</Heading>
+            <Heading level={4}>Sub-sub-heading</Heading>
+            <Section>
+              <Heading level={5}>Sub-sub-sub-heading</Heading>
+              <Heading level={5}>Sub-sub-sub-heading</Heading>
+              <Heading level={5}>Sub-sub-sub-heading</Heading>
+              <Section>
+                <Heading level={6}>Sub-sub-sub-sub-heading</Heading>
+                <Heading level={6}>Sub-sub-sub-sub-heading</Heading>
+                <Heading level={6}>Sub-sub-sub-sub-heading</Heading>
+              </Section>
+            </Section>
+          </Section>
         </Section>
-    );
+      </Section>
+    </Section>
+  );
 }
