06bdd90572f0156783d3f7c0c6a07bdf19ad033c
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-09-13 19:08:42 +0300

    using context anywhere in the screen

--- my-app/src/Managing state/Scaling-up--reducer_context/TaskList.jsx
+++ my-app/src/Managing state/Scaling-up--reducer_context/TaskList.jsx
@@ -1,19 +1,22 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { TasksContext, TasksDispatchContext } from "./TasksContext";
 
-export default function TaskList({ tasks, onChangeTask, onDeleteTask }) {
+export default function TaskList() {
+  const tasks = useContext(TasksContext);
   return (
     <ul>
       {tasks.map((task) => (
         <li key={task.id}>
-          <Task task={task} onChange={onChangeTask} onDelete={onDeleteTask} />
+          <Task task={task} />
         </li>
       ))}
     </ul>
   );
 }
 
-function Task({ task, onChange, onDelete }) {
+function Task({ task }) {
   const [isEditing, setIsEditing] = useState(false);
+  const dispatch = useContext(TasksDispatchContext);
   let taskContent;
   if (isEditing) {
     taskContent = (
@@ -21,9 +24,12 @@ function Task({ task, onChange, onDelete }) {
         <input
           value={task.text}
           onChange={(e) => {
-            onChange({
-              ...task,
-              text: e.target.value,
+            dispatch({
+              type: "changed",
+              task: {
+                ...task,
+                text: e.target.value,
+              },
             });
           }}
         />
@@ -45,14 +51,22 @@ function Task({ task, onChange, onDelete }) {
         type="checkbox"
         checked={task.done}
         onChange={(e) => {
-          onChange({
-            ...task,
-            done: e.target.checked,
+          dispatch({
+            type: "changed",
+            task: {
+              ...task,
+              done: e.target.checked,
+            }
           });
         }}
       />
       {taskContent}
-      <button onClick={() => onDelete(task.id)}>Delete</button>
+      <button onClick={() => dispatch({
+        type: 'deleted',
+        id: task.id
+      })}
+      
+      >Delete</button>
     </label>
   );
 }
