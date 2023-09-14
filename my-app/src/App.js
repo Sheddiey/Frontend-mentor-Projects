d6d3a61d56030a5e91bee8e034775b4babdd1cc8
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-09-14 13:42:29 +0300

    scrolling to an element

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,5 @@
-import Form from "./Escape Hatches/Manipulating DOM with Ref/Form";
-import Counter from "./Escape Hatches/Referencing values with ref/Counter";
-import CounterButton from "./Escape Hatches/Referencing values with ref/CounterButton";
-import StopWatch from "./Escape Hatches/Referencing values with ref/StopWatch";
+import CatFriends from "./Escape Hatches/Manipulating DOM with Ref/CatFriends";
 
 export default function App() {
-  return (
-    <>
-      <Counter />
-      <hr />
-      <StopWatch />
-      <hr />
-      <CounterButton />
-      <hr />
-      <Form />
-    </>
-  );
+  return <CatFriends />;
 }
