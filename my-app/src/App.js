c7fe42673d182c3f3177058a2042beb480a17d9d
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-09-14 13:12:19 +0300

    accessing the DOM with refs

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,3 +1,4 @@
+import Form from "./Escape Hatches/Manipulating DOM with Ref/Form";
 import Counter from "./Escape Hatches/Referencing values with ref/Counter";
 import CounterButton from "./Escape Hatches/Referencing values with ref/CounterButton";
 import StopWatch from "./Escape Hatches/Referencing values with ref/StopWatch";
@@ -6,8 +7,12 @@ export default function App() {
   return (
     <>
       <Counter />
+      <hr />
       <StopWatch />
+      <hr />
       <CounterButton />
+      <hr />
+      <Form />
     </>
   );
 }
