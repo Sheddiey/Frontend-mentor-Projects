9d281cb1bc33a5bc8b81e2f3a5729aa06965eeb5
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-09-14 12:13:11 +0300

    added a stop button

--- my-app/src/Escape Hatches/Referencing values with ref/StopWatch.jsx
+++ my-app/src/Escape Hatches/Referencing values with ref/StopWatch.jsx
@@ -1,16 +1,22 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export default function StopWatch() {
   const [startTime, setStartTime] = useState(null);
   const [now, setNow] = useState(null);
+  const intervalRef = useRef(null);
 
   function handleStart() {
     setStartTime(Date.now());
     setNow(Date.now());
 
-    setInterval(() => {
+    clearInterval(intervalRef.current);
+    intervalRef.current = setInterval(() => {
       setNow(Date.now());
     }, 1);
+  }
+
+  function handleStop() {
+    clearInterval(intervalRef.current);
   }
 
   let secondsPassed = 0;
@@ -22,6 +28,7 @@ export default function StopWatch() {
     <>
       <h1>Time passed: {secondsPassed.toFixed(100)}</h1>
       <button onClick={handleStart}>Start</button>
+      <button onClick={handleStop}>Stop</button>
     </>
   );
 }
