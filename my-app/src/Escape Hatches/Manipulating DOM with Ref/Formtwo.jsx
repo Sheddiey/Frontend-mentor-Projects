38b0ae6be91f1330ea6c3c01e3966f61286fbc23
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-09-14 14:38:00 +0300

    using an imperative handle

--- my-app/src/Escape Hatches/Manipulating DOM with Ref/Formtwo.jsx
+++ my-app/src/Escape Hatches/Manipulating DOM with Ref/Formtwo.jsx
@@ -1,7 +1,13 @@
-import { forwardRef, useRef } from "react";
+import { forwardRef, useImperativeHandle, useRef } from "react";
 
 const MyInput = forwardRef((props, ref) => {
-    return <input {...props} ref={ref} />
+    const realInputRef = useRef(null);
+    useImperativeHandle(ref, () => ({
+        focus() {
+            realInputRef.current.focus();
+        },
+    }));
+    return <input {...props} ref={realInputRef} />
 });
 
 export default function FormTwo() {
