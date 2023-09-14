0acfab801fda0f356439bc22dcdb9606288ba217
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-09-14 14:15:50 +0300

    using a ref callback

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
-import CatFriends from "./Escape Hatches/Manipulating DOM with Ref/CatFriends";
+import CatFriendsPartTwo from "./Escape Hatches/Manipulating DOM with Ref/CatFriendsPartTwo";
 
 export default function App() {
-  return <CatFriends />;
+  return <CatFriendsPartTwo />;
 }
