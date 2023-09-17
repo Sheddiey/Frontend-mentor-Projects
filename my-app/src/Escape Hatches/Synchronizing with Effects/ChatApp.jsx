7eadfe28161e9df078d3c35b5c2f807b0cec40df
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-09-17 19:03:20 +0300

    added a cleanup function

--- my-app/src/Escape Hatches/Synchronizing with Effects/ChatApp.jsx
+++ my-app/src/Escape Hatches/Synchronizing with Effects/ChatApp.jsx
@@ -5,6 +5,9 @@ export default function ChatRoom() {
   useEffect(() => {
     const connection = createConnection();
     connection.connect();
+    return () => {
+        connection.disconnect();
+    }
   }, []);
   return <h1>Welcome to the chat!</h1>;
 }
