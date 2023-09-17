d1134d0001d6a96a5f6d5b2ddc60e1c7539d210f
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-09-17 18:15:29 +0300

    specifying dependecies

--- my-app/src/Escape Hatches/Synchronizing with Effects/VideoPlayer.jsx
+++ my-app/src/Escape Hatches/Synchronizing with Effects/VideoPlayer.jsx
@@ -1,28 +1,34 @@
 import { useEffect, useRef, useState } from "react";
 
-function VideoPlayer({ src, isPLaying}) {
-    const ref = useRef(null);
+function VideoPlayer({ src, isPLaying }) {
+  const ref = useRef(null);
 
-    useEffect(() => {
-        if (isPLaying) {
-            ref.current.play();
-        } else {
-            ref.current.pause();
-        }
-    });
+  useEffect(() => {
+    if (isPLaying) {
+      console.log("calling video.play()");
+      ref.current.play();
+    } else {
+      console.log("calling video.pause()");
+      ref.current.pause();
+    }
+  }, [isPLaying]);
 
-return <video ref={ref} src= {src} loop playsInline/>
+  return <video ref={ref} src={src} loop playsInline />;
 }
 
 export default function VideoPlayerApp() {
-    const [isPLaying, setIsPlaying] = useState(false);
-    return (
-        <>
-            <button onClick={() => setIsPlaying(!isPLaying)}>
-                {isPLaying ? 'pause' : 'play'}
-            </button>
-            <VideoPlayer isPLaying={isPLaying} src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
-            />
-        </>
-    );
+  const [isPLaying, setIsPlaying] = useState(false);
+  const [text, setText] = useState("");
+  return (
+    <>
+      <input value={text} onChange={(e) => setText(e.target.value)} />
+      <button onClick={() => setIsPlaying(!isPLaying)}>
+        {isPLaying ? "pause" : "play"}
+      </button>
+      <VideoPlayer
+        isPLaying={isPLaying}
+        src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
+      />
+    </>
+  );
 }
