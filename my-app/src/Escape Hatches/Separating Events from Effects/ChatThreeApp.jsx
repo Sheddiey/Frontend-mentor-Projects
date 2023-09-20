715ae33bd76c156fbbb7cdb45b5f370a5d9ca0e2
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-09-20 12:04:19 +0300

    fixed object dependencies

--- my-app/src/Escape Hatches/Separating Events from Effects/ChatThreeApp.jsx
+++ my-app/src/Escape Hatches/Separating Events from Effects/ChatThreeApp.jsx
@@ -1,50 +1,43 @@
 import { useEffect, useState } from "react";
 import { createConnection } from "./ChatThree";
 
-const serverUrl = 'https://localhost:1234';
+const serverUrl = "https://localhost:1234";
 
 function ChatRoom({ roomId }) {
-    const [messages, setMessages] = useState('')
+  const [messages, setMessages] = useState("");
 
-
-    // Temporarily disable the linter to demonstrate the problem
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => {
     const options = {
-        serverUrl: serverUrl,
-        roomId: roomId
-    }
+      serverUrl: serverUrl,
+      roomId: roomId,
+    };
+    const connection = createConnection(options);
+    connection.connect();
+    return () => connection.disconnect();
+  }, [roomId]);
 
-    useEffect(() => {
-        const connection = createConnection(options);
-        connection.connect();
-        return () => connection.disconnect();
-    }, [options]);
-
-    return (
-        <>
-            <h1>Welcome to the {roomId} room!</h1>
-            <input value={messages} onChange={e => setMessages(e.target.value)} />
-        </>
-    );
+  return (
+    <>
+      <h1>Welcome to the {roomId} room!</h1>
+      <input value={messages} onChange={(e) => setMessages(e.target.value)} />
+    </>
+  );
 }
 
 export default function ChatThreeApp() {
-    const [roomId, setRoomId] = useState('general')
-    return (
-        <>
-            <label>
-                Choose the chat room:{' '}
-                <select
-                    value={roomId}
-                    onChange={e => setRoomId(e.target.value)}
-                >
-                    <option value='general'>general</option>
-                    <option value='travel'>travel</option>
-                    <option value='music'>music</option>
-                </select>
-                <hr />
-                <ChatRoom roomId={roomId} />
-            </label>
-        </>
-    );
+  const [roomId, setRoomId] = useState("general");
+  return (
+    <>
+      <label>
+        Choose the chat room:{" "}
+        <select value={roomId} onChange={(e) => setRoomId(e.target.value)}>
+          <option value="general">general</option>
+          <option value="travel">travel</option>
+          <option value="music">music</option>
+        </select>
+        <hr />
+        <ChatRoom roomId={roomId} />
+      </label>
+    </>
+  );
 }
