99e651f56d1e12bd8b071b976ecd29bf25ab3af3
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-09-22 19:09:15 +0300

    fixed image issues

--- time-tracking-dashboard-react/src/components/Card.jsx
+++ time-tracking-dashboard-react/src/components/Card.jsx
@@ -137,15 +137,17 @@ export function Card() {
             key={data.title}
             style={{ backgroundColor: backgroundColor }}
           >
-            <img className="bg-icon" src={data.src} alt={data.title} />
+            <div className="bg-icon-container">
+              <img className="bg-icon" src={data.src} alt={data.title} />
+            </div>
             <div className="dark-blue">
               <div className="flex-space-between">
                 <p>{data.title}</p>
                 <img src={ellipsis} alt="icon-ellipsis" />
               </div>
               <div className="flex-mobile">
-                <h1>{data.timeframes.weekly.current} hrs</h1>
-                <p>Last Week - {data.timeframes.weekly.previous} hrs</p>
+                <h1>{data.timeframes.weekly.current}hrs</h1>
+                <p>Last Week - {data.timeframes.weekly.previous}hrs</p>
               </div>
             </div>
           </div>
